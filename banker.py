497b4b900ad611a3b38408e97b00612e22c2889d
Nayan <75008442+Nayan-das08@users.noreply.github.com>
2022-03-22 14:51:59 +0530

    Update banker.py finished Request Allocation Algo

--- banker.py
+++ banker.py
@@ -76,23 +76,110 @@ temp = need[:]
 
 print()
 
-
-for i in range(n):
-#i = 0
-    if (finish[i] == False):    
-        flag = 1
-        for j in range(m):
-            if need[i][j] > work[j]:
-                flag = 0
-                print(i,j)
-                #break        
-        
-        if flag == 1:   # found P[i] where finish[i] is false and need[i] < work
-            print(f'P{i}', end='')
+iterations = 0
+safe = 0
+while (iterations < 10) and (False in finish):
+    for i in range(n):
+    #i = 0
+        if (finish[i] == False):    
+            flag = 1
             for j in range(m):
-                work[j] = work[j] + allocation[i][j]
-            finish[i] = True
-            print(f'\t{work}')
+                if need[i][j] > work[j]:
+                    flag = 0
+                    print(f"ignored : {i,j}")
+                    #break        
+            
+            if flag == 1:   # found P[i] where finish[i] is false and need[i] < work
+                print(f'P{i}', end='')
+                for j in range(m):
+                    work[j] = work[j] + allocation[i][j]
+                finish[i] = True
+                print(f'\t{work}')
+    
+    print(f"after {iterations+1} iterations",end='')
+    if False in finish:
+        print("\tFalse in finish[]")
+    else:
+        print("\tAll True in finish[]")
+        safe = 1
+    iterations += 1
+
+if safe == 1:
+    print("\n\nThe system is in SAFE STATE!")
+
+#------------------------------------------------------------
+
+print('\n')
+request = []
+x = int(input("Enter Process ID which is requesting resources : "))
+
+print(f"\nRequesting p{x}")
+for i in range(m):
+    print(f"\tEnter instances of R{i} : ", end='')
+    z = int(input())
+    request.append(z)
+
+
+for i in range(m):
+    # step 1
+    if request[i] <= need[x][i]:
+        # step 2
+        if request[i] <= available[i]:
+            # step 3
+            for j in range(m):
+                available[j]  = available[j] - request[j]
+                allocation[x][j] = allocation[x][j] + request[j]
+                need[x][j] = need[x][j] - request[j]
+            # now perform safety algo
+        else:
+            print(f"P{x} needs to wait as resources are not available")
+            exit
+    else:
+        print("Request is more than needed instances")
+        exit
+
+# SAFETY ALGORITHM after Recource-Request Allocation
+work = available[:]
+finish = [False]*n
+temp = need[:]
+
+print()
+
+iterations = 0
+safe = 0
+while (iterations < 10) and (False in finish):
+    for i in range(n):
+    #i = 0
+        if (finish[i] == False):    
+            flag = 1
+            for j in range(m):
+                if need[i][j] > work[j]:
+                    flag = 0
+                    print(f"ignored : {i,j}")
+                    #break        
+            
+            if flag == 1:   # found P[i] where finish[i] is false and need[i] < work
+                print(f'P{i}', end='')
+                for j in range(m):
+                    work[j] = work[j] + allocation[i][j]
+                finish[i] = True
+                print(f'\t{work}')
+    
+    print(f"after {iterations+1} iterations",end='')
+    if False in finish:
+        print("\tFalse in finish[]")
+    else:
+        print("\tAll True in finish[]")
+        safe = 1
+    iterations += 1
+
+if safe == 1:
+    print(f"\n\nThe system is in SAFE STATE!")
+    print(f"request {request} CAN be allocated")
+else:
+    print(f"\n\nThe system is NOT in SAFE STATE!")
+    print(f"request {request} CANNOT be allocated")
+
 
 5
 4
