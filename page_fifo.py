5a115f59a1128ba3af24e38aa2efeeafd03116d0
Nayan <nayanpcstudy@gmail.com>
2022-03-29 14:36:12 +0530

    corrected fifo

--- page_fifo.py
+++ page_fifo.py
@@ -5,18 +5,24 @@ for i in range(m):
     x = int(input())
     ref.append(x)
 
+#ref = [1,2,3,4,2,1,5,6,2,1,2,3,7,6,3,2,1,2,3,6]
 
 n = int(input("\nEnter number of free frames : "))
 frames = [0]*n
-
+j = 0
 count = 0 
 for i in range(len(ref)):
     print(f"{ref[i]}", end='')
     if ref[i] not in frames:
-        frames.pop(0)
-        frames.append(ref[i])
+        #frames.pop(0)
+        #frames.append(ref[i])
+        frames[j] = ref[i]
         print(f"\tpage fault \t{frames}")
+        
         count += 1
+        j += 1
+        if j == 3:
+            j = 0
     else:
         print()
 
